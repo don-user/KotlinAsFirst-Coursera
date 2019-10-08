613edd5734d35a6c040aab621fcd8b0abad3295c
Yuriy <don_1303@mail.ru>
2019-10-08 13:34:17 +0400

    Lesson 2 test task 2

--- test/lesson2/task2/Tests.kt
+++ test/lesson2/task2/Tests.kt
@@ -20,6 +20,7 @@ class Tests {
         assertTrue(isNumberHappy(1533))
         assertTrue(isNumberHappy(9009))
         assertFalse(isNumberHappy(3644))
+        assertFalse(isNumberHappy(7000))
     }
 
     @Test
@@ -28,6 +29,7 @@ class Tests {
         assertTrue(queenThreatens(3, 6, 7, 6))
         assertTrue(queenThreatens(8, 1, 1, 8))
         assertFalse(queenThreatens(7, 6, 5, 7))
+        assertFalse(queenThreatens(1, 1, 2, 3))
     }
 
     @Test
@@ -45,6 +47,9 @@ class Tests {
         assertEquals(29, daysInMonth(2, 1996))
         assertEquals(28, daysInMonth(2, 1900))
         assertEquals(29, daysInMonth(2, 2000))
+        assertEquals(29, daysInMonth(2, 1600))
+        assertEquals(28, daysInMonth(2, 1700))
+        assertEquals(31, daysInMonth(12, 53))
     }
 
     @Test
@@ -55,6 +60,7 @@ class Tests {
         assertTrue(circleInside(2.0, 2.0, 2.0, 2.0, 2.0, 2.0))
         assertTrue(circleInside(-2.0, 3.0, 2.0, -2.0, 0.0, 5.0))
         assertFalse(circleInside(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+        assertTrue(circleInside(2.0, 3.0, 3.0, 3.0, 3.0, 7.0))
     }
 
     @Test
@@ -63,6 +69,8 @@ class Tests {
         assertTrue(brickPasses(2, 10, 5, 6, 3))
         assertTrue(brickPasses(4, 4, 4, 4, 4))
         assertFalse(brickPasses(6, 5, 4, 3, 6))
+        assertFalse(brickPasses(1, 8, 5, 3, 1))
         assertTrue(brickPasses(3, 2, 1, 1, 2))
+        assertFalse(brickPasses(10, 15, 5, 20, 2))
     }
 }
